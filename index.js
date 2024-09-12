3f36975a949b994506336c7da33fca36d3be0fb7
HimanshuHeda <harsh967896@gmail.com>
2024-09-12 09:44:20 +0530

    files

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+const math = require("./math.js");
+
+console.log("Hello World");
+
+console.log(math.addition(5, 9)); // Use math.addition
+console.log(math.sub(8, 9));      // Use math.sub
+console.log(math.mul(5, 10));     // Use math.mul
+console.log(math.div(9, 4));      // Use math.div
+
+
+// node index
+// npm init
+// npm start
---
+++ math.js
@@ -0,0 +1,18 @@
+function addition(a,b){
+    return a + b;
+}
+
+function sub(a,b){
+    return a - b;
+}
+
+function mul(a,b){
+    return a * b;
+}
+
+function div(a,b){
+    return a / b;
+}
+// console.log(addition(5,9));
+module.exports = {addition, sub, mul, div};
+
