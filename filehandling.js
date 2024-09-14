3413a0922ede2bf2b1ed970492b9a0a0aa0c0432
HimanshuHeda <harsh967896@gmail.com>
2024-09-14 15:02:01 +0530

    text_file

---
+++ filehandling.js
@@ -0,0 +1,25 @@
+const { error } = require("console");
+const fss = require("fs");
+const { writeFile } = require("fs/promises");
+
+fss.writeFileSync("./test.txt","Testing.....");
+fss.writeFile("./test1.txt","Testing...",(error)=>{
+    console.log("Some error has occurred: " + error);
+});
+
+fss.appendFileSync("./app.txt","Testing.....");
+
+fss.appendFile("./text1.txt","Testing...",(error)=>{
+    console.log("Some error has occurred: " + error);
+});
+
+fss.readFile("./test.txt","utf-8",(err,result)=>{
+    if(err){
+        console.log("Some error has occurred: " + err);
+    }
+    else{
+        console.log(result);
+    }
+})
+
+let result = fss.readFileSync("./test1.txt","utf-8");
